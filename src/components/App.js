df0f0ccfd252389d9d2f07528df6e9b03d7eea09
Witold Olszyk <witoldolszyk@gmail.com>
2018-03-18 01:06:33 +0100

    add

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title.js'
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+    removeTodo(id) {
+      const remainder = this.state.data.filter(todo => todo.id !== id);
+      this.setState({data: remainder});
+    }
+
+
+    render() {
+      return (
+          <div className={style.TodoApp}>
+            <Title title={'tutaj będzie dodawany tytuł'}>
+            </Title>
+          </div>
+      );
+    }
+}
+
+export default App;
